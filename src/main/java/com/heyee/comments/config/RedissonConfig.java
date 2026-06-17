69293e6c2a1346d45ce957bd3935fe89e79ac488
heyi664 <pengyuw1006@gmail.com>
2026-06-17 20:50:44 +0800

    Add heyee comments full stack project

--- src/main/java/com/heyee/comments/config/RedissonConfig.java
+++ src/main/java/com/heyee/comments/config/RedissonConfig.java
@@ -3,18 +3,30 @@ package com.heyee.comments.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class RedissonConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password:}")
+    private String redisPassword;
+
     @Bean
-    public RedissonClient redissonClient(){
-        // 配置
+    public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.23.129:6379").setPassword("123321");
-        // 创建RedissonClient对象
+        config.useSingleServer().setAddress("redis://" + redisHost + ":" + redisPort);
+        if (StringUtils.hasText(redisPassword)) {
+            config.useSingleServer().setPassword(redisPassword);
+        }
         return Redisson.create(config);
     }
 }
